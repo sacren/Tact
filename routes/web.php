1f7f0b430e748221c7ec46c60b931c799fc00de7
Jean Sacren <sakiwit@gmail.com>
2025-03-11 21:58:07 -0600

    Add routes for different data types
    In addition to Blade view, build the routes of get request by using a
    text string or array for response.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return 'Welcome to Dashboard';
+});
+
+Route::get('/users', function () {
+    return [
+        'Pythagoras',
+        'Geoffrey Chaucer',
+        '阿久津',
+    ];
+});
