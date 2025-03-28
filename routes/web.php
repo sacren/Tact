eb79af5aa3e889b326a15c6e0336b262c5e57b82
Jean Sacren <sakiwit@gmail.com>
2025-03-28 14:42:39 -0600

    Use chaining for both prefix() and name()
    Both prefix and name methods can be chained together and get rid of
    unnecessary nesting.

--- routes/web.php
+++ routes/web.php
@@ -30,22 +30,20 @@ Route::get('/tact', function () {
 
 Route::redirect('/home', '/');
 
-Route::prefix('transactions')->group(function () {
-    Route::name('transactions.')->group(function () {
-        Route::controller(TransactionController::class)->group(function () {
-            Route::get('/', 'index')->name('index');
-            Route::get('/create', 'create')->name('create');
-            Route::post('/', 'store')->name('store');
-            Route::get('/{transactionId}', 'show')->name('show');
-            Route::get('/{transactionId}/edit', 'edit')->name('edit');
-            Route::patch('/{transactionId}', 'update')->name('update');
-            Route::delete('/{transactionId}', 'destroy')->name('destroy');
+Route::prefix('transactions')->name('transactions.')->group(function () {
+    Route::controller(TransactionController::class)->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/create', 'create')->name('create');
+        Route::post('/', 'store')->name('store');
+        Route::get('/{transactionId}', 'show')->name('show');
+        Route::get('/{transactionId}/edit', 'edit')->name('edit');
+        Route::patch('/{transactionId}', 'update')->name('update');
+        Route::delete('/{transactionId}', 'destroy')->name('destroy');
 
-            Route::get('/{transactionId}/files/{fileId?}', 'showFile')->name('showfile');
-        });
-
-        Route::get('/{transactionId}/process', ProcessController::class)->name('process');
+        Route::get('/{transactionId}/files/{fileId?}', 'showFile')->name('showfile');
     });
+
+    Route::get('/{transactionId}/process', ProcessController::class)->name('process');
 });
 
 Route::get('/report/{year}/{month?}', [ReportController::class, 'showYearMonth']);
