e89fde9ba7428bd34bebb6e67a5fde79736a3277
Jean Sacren <sakiwit@gmail.com>
2025-03-28 19:12:41 -0600

    Build new discrete routing for transactions routes
    Create a new file named transactions.php in the routes directory.  This
    file will contain all the transaction-related routes.
    
    Customize the withRouting method in the bootstrap/app.php file.  This
    method is part of Laravel's application bootstrapping process and
    defines custom routing logic for Laravel application.  The web argument
    is now an array that includes both web.php and transactions.php.  This
    tells Laravel to load both files as part of the web routes.
    
    Before and after the use of new routing, artisan route:list affirms the
    transactions routes remain the same.
    
    $ sail artisan route:list --path=transactions

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Enums\FiledBy;
-use App\Http\Controllers\ProcessController;
 use App\Http\Controllers\ReportController;
-use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
@@ -29,22 +27,6 @@ Route::get('/tact', function () {
 });
 
 Route::redirect('/home', '/');
-
-Route::prefix('transactions')->name('transactions.')->group(function () {
-    Route::controller(TransactionController::class)->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::get('/create', 'create')->name('create');
-        Route::post('/', 'store')->name('store');
-        Route::get('/{transactionId}', 'show')->name('show');
-        Route::get('/{transactionId}/edit', 'edit')->name('edit');
-        Route::patch('/{transactionId}', 'update')->name('update');
-        Route::delete('/{transactionId}', 'destroy')->name('destroy');
-
-        Route::get('/{transactionId}/files/{fileId?}', 'showFile')->name('showfile');
-    });
-
-    Route::get('/{transactionId}/process', ProcessController::class)->name('process');
-});
 
 Route::get('/report/{year}/{month?}', [ReportController::class, 'showYearMonth']);
 
