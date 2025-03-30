be8b22a72828ba61a910ec132ceecee4aeac075e
Jean Sacren <sakiwit@gmail.com>
2025-03-29 19:15:27 -0600

    Prepend AssignRequestId custom middleware
    In global middleware context prepend the custom middleware rather than
    append it.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware
-            ->append(AssignRequestId::class);
+            ->prepend(AssignRequestId::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
