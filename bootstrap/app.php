b960d904f769c20518ca5121490f0746dcb05737
Jean Sacren <sakiwit@gmail.com>
2025-04-01 18:57:22 -0600

    Use append named argument for web method

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        return $middleware->web(prepend: [
+        return $middleware->web(append: [
             AssignRequestId::class,
             AddColor::class,
         ]);
