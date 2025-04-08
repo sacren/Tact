23ae753874c9bc8585352ac817c4ac3f54bab562
Jean Sacren <sakiwit@gmail.com>
2025-04-08 01:46:15 -0600

    Use bind with a closure for custom logic
    This is an imperative approach that explicitly defines how the binding
    should be resolved.  The closure can include custom logic to resolve the
    dependency with more room for control.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,19 +10,16 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register bindings in the container.
-     */
-    public array $bindings = [
-        PaymentProcessor::class => Stripe::class,
-    ];
-
-    /**
      * Register any application services.
      */
     public function register(): void
     {
         $this->app->singleton(TransactionService::class, function () {
             return new TransactionService();
+        });
+
+        $this->app->bind(PaymentProcessor::class, function () {
+            return new Stripe();
         });
     }
 
