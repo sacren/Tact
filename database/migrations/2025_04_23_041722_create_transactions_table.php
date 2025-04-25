d977bf98615e153c668bae3c6165e9acae959f9e
Jean Sacren <sakiwit@gmail.com>
2025-04-24 18:49:08 -0600

    Create a new string column on the table
    In Laravel's Schema Builder, the string method is used to define a
    column that stores textual data.  It is one of the most commonly used
    column types for storing short strings, such as names, titles,
    descriptions, or other text-based information.  The maximum number of
    characters the column can store defaults to 255.

--- database/migrations/2025_04_23_041722_create_transactions_table.php
+++ database/migrations/2025_04_23_041722_create_transactions_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
                   ->cascadeOnDelete()
                   ->cascadeOnUpdate();
             $table->decimal('amount', 10);
+            $table->string('description');
             $table->timestamps();
         });
     }
